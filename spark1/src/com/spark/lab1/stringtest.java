51a2f575d07f1969145067387f881e1ecc162c2a
Niwash <Niwash@IBM578-R9019EMX>
2018-04-06 23:34:20 +0530

    niwas commit 2

--- spark1/src/com/spark/lab1/stringtest.java
+++ spark1/src/com/spark/lab1/stringtest.java
@@ -5,7 +5,7 @@ public class stringtest {
 	public static void main(String[] args) {
 		
 		String str="hi guys how are you";
-		String str3="hi";
+		String str3="hii";
 		String str1=str.substring(1,8);
 		System.out.println(str.toUpperCase());
 		System.out.println(str);
